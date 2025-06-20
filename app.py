95c5642a06a56891bee20dda0d6e9cc530b79439
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-21 03:23:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-import os
-from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
+import asyncio
+import os
 from handlers.start import start_handler
 from handlers.menu import menu_handler
 from handlers.help import help_handler
 
-load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-def main():
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    # Register command handlers
-    app.add_handler(CommandHandler("start", start_handler))
-    app.add_handler(CommandHandler("menu", menu_handler))
-    app.add_handler(CommandHandler("help", help_handler))
+    # Tambahkan handler
+    app.add_handler(start_handler)
+    app.add_handler(menu_handler)
+    app.add_handler(help_handler)
 
-    print("✅ Bot is running via polling...")
-    app.run_polling()
+    # Start bot
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+    await app.updater.idle()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
