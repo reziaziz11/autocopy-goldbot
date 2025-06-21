24298f64b85138f1c2533bcd88ad772743b515a8
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-21 10:31:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
+import asyncio
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
-from telegram.ext import ApplicationBuilder
-from handlers.start import start_handler
-from handlers.menu import menu_handler
-from handlers.help import help_handler
 from dotenv import load_dotenv
 
 load_dotenv()
-
 TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot aktif gan 🚀")
 
 async def main():
     app = ApplicationBuilder().token(TOKEN).build()
-
-    app.add_handler(start_handler)
-    app.add_handler(menu_handler)
-    app.add_handler(help_handler)
-
-    print("Bot started...")
-    await app.run_polling()  # ✅ Cara baru, nggak ada .updater lagi
+    app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
