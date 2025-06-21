ebb14db705c81d1c31d0f0d132f5d4cb661143b0
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-21 11:24:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import asyncio
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")  # Sesuai key yang kamu pakai di Render
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot aktif gan 🚀")
