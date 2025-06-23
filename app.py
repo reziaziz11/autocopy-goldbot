4b20e5dc98cc05cf21da0c096e7b0decb28e522b
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-23 15:50:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from flask import Flask, request
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 from bot.handlers import start_handler
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # === Setup Bot ===
 TOKEN = os.getenv("TOKEN")
 WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # misalnya https://djgoldbot.onrender.com/webhook
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # contoh: https://djgoldbot.onrender.com/webhook
 
 application = ApplicationBuilder().token(TOKEN).build()
 application.add_handler(CommandHandler("start", start_handler))
@@ -24,5 +24,10 @@ def home():
 def webhook():
     if request.method == "POST":
         update = Update.de_json(request.get_json(force=True), application.bot)
-        asyncio.run(application.process_update(update))
+
+        async def process():
+            await application.initialize()  # WAJIB sebelum process_update
+            await application.process_update(update)
+
+        asyncio.run(process())
         return "OK", 200
