3d59499a9693c104972b4b9499f0e84f08161602
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-23 17:35:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,34 @@ import os
 from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
-
 from bot.handlers import start_handler
+from dotenv import load_dotenv
 
+# Load token & env
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+WEBHOOK_PATH = "/webhook"
+
+# Init Flask
 app = Flask(__name__)
 
-TOKEN = os.getenv("TOKEN")
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-# Setup Bot
+# Init Telegram App
 application = ApplicationBuilder().token(TOKEN).build()
 application.add_handler(CommandHandler("start", start_handler))
-application.initialize()  # jangan lupa initialize
 
-# Set webhook saat startup
-@app.before_first_request
+# Set webhook manual
 def set_webhook():
     application.bot.set_webhook(WEBHOOK_URL)
 
+set_webhook()
+
 @app.route("/", methods=["GET"])
 def home():
-    return "✅ DJGOLD_BOT is running!"
+    return "✅ DJGOLD_BOT aktif!"
 
 @app.route(WEBHOOK_PATH, methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), application.bot)
-    application.process_update(update)  # sync processing
+    application.process_update(update)
     return "OK", 200
-
-if __name__ == "__main__":
-    app.run()
