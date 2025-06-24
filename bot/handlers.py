5f6229753cbfe9c334fd472dd7271642f0ad825a
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-24 17:37:39 +0700

    Delete bot directory

--- bot/handlers.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot aktif dan siap menerima perintah!")
