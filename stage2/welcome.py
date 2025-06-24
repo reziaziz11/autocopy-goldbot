4b8f05edea292dcc4b3f1dac2377757e1592d936
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-24 17:37:49 +0700

    Delete stage2 directory

--- stage2/welcome.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
-
-async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot aktif dan siap menerima perintah!")
-
-welcome_handler = CommandHandler("start", welcome)
