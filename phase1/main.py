53c3b1f6fbb8a9fd5137f448a407330a627eb1fb
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-25 15:44:28 +0700

    Update main.py

--- phase1/main.py
+++ phase1/main.py
@@ -1,12 +1,31 @@
 from flask import Flask, request
-import os
+from telegram import Update, Bot
+from telegram.ext import Dispatcher, CommandHandler
 
+TOKEN = "7524328423:AAFPrLxZtxnyyGmmguhc5KU_e524xnq4thI"
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher(bot, None, use_context=True)
+
+# Handler
+def start(update, context):
+    update.message.reply_text("Bot aktif!")
+
+dispatcher.add_handler(CommandHandler("start", start))
+
+# Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Phase 1 aktif!'
+    return 'Bot aktif di root'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return 'OK'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
