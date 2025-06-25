b2323463ff0347da03ef9337e60429180bb0326d
reziaziz11 <54319032+reziaziz11@users.noreply.github.com>
2025-06-25 15:29:11 +0700

    Update main.py

--- phase1/main.py
+++ phase1/main.py
@@ -1,28 +1,12 @@
 from flask import Flask, request
-import requests
+import os
 
 app = Flask(__name__)
 
-TOKEN = "7524328423:AAFPrLxZtxnyyGmmguhc5KU_e524xnq4thI"
-URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+@app.route('/')
+def index():
+    return 'Phase 1 aktif!'
 
-@app.route("/")
-def home():
-    return "Bot aktif."
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    data = request.get_json()
-    print("DATA MASUK:", data)  # Debug log akan tampil di Render Logs
-
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        text = data["message"].get("text", "")
-        reply = f"📩 Kamu mengirim: {text}"
-
-        requests.post(URL, json={
-            "chat_id": chat_id,
-            "text": reply
-        })
-
-    return {"ok": True}
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
